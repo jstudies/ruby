8549684b5aa66da2ff0745a5755b7b7bd0ffcd7f
jstudies <mdpmcm@gmail.com>
2018-11-22 11:28:57 +0200

    Initial commit.

---
+++ test.rb
@@ -0,0 +1,31 @@
+book_1 = "A Wrinkle in Time"
+
+book_2 = "A Brief History of Time"
+
+print book_1 <=> book_2
+
+
+# books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+# books.sort!
+# books.each{|i| puts i}
+
+# my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
+# my_array.sort!
+# my_array.each{|i| puts i}
+
+
+
+
+
+# def greeter(name)
+#   return "Welcome #{name}!"
+# end
+#
+# def by_three?(i)
+#   if i%3==0
+#     return true
+#     else return false
+#   end
+#   end
+#
+# print by_three?(8)
